f0520b9358a9b7a9ac26899fae2117f9377fdbbf
aepnat <aep.nat@gmail.com>
2018-11-16 08:22:02 +0700

    dynamic menus from voyager

---
+++ resources/views/partials/menus/footer.blade.php
@@ -0,0 +1,10 @@
+<ul>
+    <li>Follow Me: </li>
+    @foreach($items as $menu_item)
+        <li>
+            <a href="{{ $menu_item->link() }}">
+                <i class="fa {{ $menu_item->title }}"></i>
+            </a>
+        </li>
+    @endforeach
+</ul>
